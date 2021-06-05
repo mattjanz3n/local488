f33a121fe397515863563329b44186981f497d4a
Bruno Ribarić <bruno@ribarich.me>
2021-06-05 22:48:51 +0200

    Page Blocks v3.0.1

--- src/page-blocks/plugin.php
+++ src/page-blocks/plugin.php
@@ -1,11 +1,10 @@
 <?php
 /**
  * Plugin Name: Page Blocks — CGB Gutenberg Block Plugin
- * Plugin URI: https://github.com/ahmadawais/create-guten-block/
  * Description: Page Blocks — is a Gutenberg plugin created via create-guten-block.
  * Author: mrahmadawais, maedahbatool
  * Author URI: https://AhmadAwais.com/
- * Version: 3.0.0
+ * Version: 3.0.1
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
